35826ba93ca457fe4b3ded269a606f44d136771f
Wesley Ramos <wesley-ramos-de-paula@hotmail.com>
2019-05-24 17:09:31 -0300

    adicionando healthcheck

--- webapp/src/main/java/br/com/developertips/api/SimpleApi.java
+++ webapp/src/main/java/br/com/developertips/api/SimpleApi.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.elastic.apm.opentracing.ElasticApmTracer;
+import io.opentracing.Scope;
+import io.opentracing.Tracer;
+
 @RestController
 @RequestMapping("/api")
 public class SimpleApi {
@@ -15,5 +19,10 @@ public class SimpleApi {
 	@GetMapping(value= "/")
 	public void api() {	
 		logger.error("Log de exemplo ");
+		
+		Tracer tracer = new ElasticApmTracer();
+	    try (Scope scope = tracer.buildSpan("say-hello").startActive(true)) {
+	        scope.span().setTag("hello-to", "wesley");
+	    }
 	}
 }
